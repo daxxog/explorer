be2c876857a51031fb16831625ea15e09947411e
David Volm <david@volminator.com>
2018-01-17 13:29:20 -0600

    sync2 bootstrap

--- sync-bootstrap.sh
+++ sync-bootstrap.sh
@@ -8,4 +8,15 @@ if [ ! -f sync.sh ]; then
 	chmod +x sync.sh
 fi
 
+if [ ! -f sync2.sh ]; then
+	echo bootstrap sync2..
+	echo \#!/bin/sh >> sync2.sh
+	echo echo syncing blockchain.. >> sync2.sh
+	echo cd $(pwd) >> sync2.sh
+	echo rm -f tmp/index.pid \&\& node --stack-size=100000 scripts/peers.js >> sync2.sh
+	echo rm -f tmp/index.pid \&\& node --stack-size=100000 scripts/sync.js market update >> sync2.sh
+	chmod +x sync2.sh
+fi
+
+./sync2.sh
 ./sync.sh
--- sync2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f tmp/index.pid && node --stack-size=100000 scripts/peers.js
-rm -f tmp/index.pid && node --stack-size=100000 scripts/sync.js market update
